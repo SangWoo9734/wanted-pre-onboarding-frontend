31b5c15a9f2a0a5a4fefb237a272cabb3b9e0a4e
Parksangwoo <psu8430@gmail.com>
2023-04-14 14:03:31 +0900

    Feat: Set page route info

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "../pages/sign_in";
+import SignUp from "../pages/sign_up/SignUp";
+import Todo from "../pages/todo";
+
+function ProjectRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>404: NOT FOUND</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default ProjectRoutes;
